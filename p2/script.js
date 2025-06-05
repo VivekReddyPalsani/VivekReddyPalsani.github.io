4e424767ff0c6c61275627b70c78e28b63f9f82f
VivekReddyPalsani <138858031+VivekReddyPalsani@users.noreply.github.com>
2025-06-05 19:33:19 +0530

    .

--- p2/script.js
+++ p2/script.js
@@ -4,7 +4,7 @@ const showLogin = () => {
     <h1>Login Form</h1>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
+    <p><button onclick = 'showHome()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
@@ -20,6 +20,15 @@ const showRegister = () => {
     <p><input type="password" id="txtPass"></p>
     <p><button>Register</button></p>
     <p><button onclick = 'showLogin()'>Login to Existing Account</button></p>
+    </div>
+    `
+    root.innerHTML = str
+}
+
+const showHome = () => {
+    let str = `
+    <div class='App-Container'>
+    <h1>WELCOME</h1>
     </div>
     `
     root.innerHTML = str
