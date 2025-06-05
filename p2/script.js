4d9ab90644531eaffc47094be81cc379a504354d
VivekReddyPalsani <138858031+VivekReddyPalsani@users.noreply.github.com>
2025-06-05 19:40:31 +0530

    .

--- p2/script.js
+++ p2/script.js
@@ -1,11 +1,13 @@
 const showLogin = () => {
     let str=`
     <div class='App-Container'>
+    <center>
     <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
+    <p><input type="text" id="txtEmail" placeholder = 'Enter your Email'></p>
+    <p><input type="password" id="txtPass" placeholder = 'Enter your password'></p>
     <p><button onclick = 'showHome()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
+    </center>
     </div>
     `
     root.innerHTML = str
@@ -14,12 +16,14 @@ const showLogin = () => {
 const showRegister = () => {
     let str=`
     <div class='App-Container'>
+    <center>
     <h1>Register Form</h1>
-    <p><input type="text" id="txtName"></p>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
+    <p><input type="text" id="txtName" placeholder = 'Enter your name'></p>
+    <p><input type="text" id="txtEmail" placeholder = 'Enter your Email'></p>
+    <p><input type="password" id="txtPass" placeholder = 'Enter your password'></p>
     <p><button>Register</button></p>
     <p><button onclick = 'showLogin()'>Login to Existing Account</button></p>
+    </center>
     </div>
     `
     root.innerHTML = str
@@ -28,7 +32,7 @@ const showRegister = () => {
 const showHome = () => {
     let str = `
     <div class='App-Container'>
-    <h1>WELCOME</h1>
+    <center><h1>WELCOME</h1></center>
     </div>
     `
     root.innerHTML = str
